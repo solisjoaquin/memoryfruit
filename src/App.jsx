9a1f3679d482a5444d00fec8a05d500f5cb0356b
Joaquin <sol19008@byui.edu>
2021-03-20 15:33:38 -0300

    agregando comentarios en App.js

--- src/App.jsx
+++ src/App.jsx
@@ -4,24 +4,41 @@ import "./styles.css";
 import data from "./cardsData";
 import styled from "styled-components";
 export default function App() {
-
+  // array con cada icono 
   const [cards, setCards] = useState(data);
+
+  // array con cartas seleccionadas
   const [selectedCards, setSelectedCards] = useState([]);
+
+  // booleano que cuando las cartas son iguales se vuelve true y agrega al array de cartas acertadas 
   const [gameWon, setGameWon] = useState(false);
+
+  // Cantidad de intentos
   const [intentos, setIntentos] = useState(0);
 
+  // Cuando selecciono 2 cartas se da medio segundo y luego se vuelven a dar vuelta
+  // se vuelve a repetir cuando selectedCards cambia.
   useEffect(() => {
     if (selectedCards.length === 2) {
       setTimeout(checkCards, 500);
     }
   }, [selectedCards]);
 
+  // Controlar que las cartas sean iguales
   const checkCards = () => {
+
+    // defino dos cartas que voy a comparar
     const [firstCard, secondCard] = selectedCards;
+
+    // comparo las 2 cartas
     if (firstCard.value === secondCard.value) {
+      // defino una variable que determina que el juego termina
       let hasGameBeenWon = true;
+      // verifico que cada carta esta dada vuelta, si no es asi el juego no termina todavia
       cards.forEach((card) => (card.flipped ? null : (hasGameBeenWon = false)));
+      // si todas las cartas estan dadas vueltas, el juego termina
       setGameWon(hasGameBeenWon);
+
     } else {
       const newCards = cards.map((card) => {
         if (card.id === firstCard.id || card.id === secondCard.id) {
@@ -37,6 +54,8 @@ export default function App() {
 
     setSelectedCards([]);
   };
+
+  // 
   const flipCard = (id) => {
     const newCardData = cards.map((card) => {
       if (card.id === id && !card.flipped) {
@@ -51,6 +70,7 @@ export default function App() {
     setCards(newCardData);
   };
 
+  // Empezar de nuevo
   const handleReset = () => {
     setGameWon(false)
     cards.map(card => card.flipped = false)
@@ -59,12 +79,22 @@ export default function App() {
   }
   return (
     <div className="App">
+
       <Title>Memory fruit</Title>
-      {gameWon ? <ScoreStyle> <span className="winner">👑 </span>Intentos totales: {intentos} </ScoreStyle> : <ScoreStyle>Intentos: {intentos}</ScoreStyle>}
+
+      {gameWon ?
+        <ScoreStyle> <span className="winner">👑 </span>Intentos totales: {intentos} </ScoreStyle>
+        :
+        <ScoreStyle>Intentos: {intentos}</ScoreStyle>
+      }
 
       <GameBoard cards={cards} flipCard={flipCard} />
-      {gameWon ? <ResetButton onClick={() => { handleReset() }}>Restart</ResetButton> :
-        <Instructions>Gira dos cartas y encuentra los pares en la menor cantidad de intentos. Los intentos acertados no suman.</Instructions>}
+
+      {gameWon ?
+        <ResetButton onClick={() => { handleReset() }}>Restart</ResetButton>
+        :
+        <Instructions>Gira dos cartas y encuentra los pares en la menor cantidad de intentos. Los intentos acertados no suman.</Instructions>
+      }
 
     </div>
   );
