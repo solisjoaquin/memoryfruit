45f5f4b949df7f347a359b6cd8db76885c4af579
Joaquin <sol19008@byui.edu>
2021-03-21 03:06:24 -0300

    pwa is alive V 0.0.1

---
+++ src/sw.js
@@ -0,0 +1,8 @@
+
+
+
+importScripts(
+    'https://storage.googleapis.com/workbox-cdn/releases/6.1.1/workbox-sw.js'
+)
+
+workbox.precaching.precacheAndRoute(self.__WB_MANIFEST)
--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,35 @@
 import { defineConfig } from 'vite'
 import reactRefresh from '@vitejs/plugin-react-refresh'
+import { VitePWA } from 'vite-plugin-pwa'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [reactRefresh()]
+
+  plugins: [
+    reactRefresh(),
+    VitePWA({
+      strategies: 'injectManifest',
+      injectRegister: false,
+      injectManifest: {
+        swSrc: './src/sw.js',
+        swDest: './dist/sw.js'
+      },
+      manifest: {
+        name: "Memoryfruit",
+        short_name: "MemoryFruit",
+        start_url: "/",
+        display: "standalone",
+        background_color: "#fff",
+        description: "Practica tu memoria descubriendo los pares de frutas",
+        icons: [
+          {
+            sizes: "410x404",
+            src: "/src/favicon.svg",
+            type: "image/x-icon"
+          }],
+      },
+
+    })
+  ]
 })
+
